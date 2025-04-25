9b1221e72ffe8dbcff880f1ab163b4003a2be50c
Diana Motta <dianarosamotta@gmail.com>
2025-04-25 02:14:07 -0300

    Criação de variaveis Globais e Components

---
+++ src/components/Heading.tsx
@@ -0,0 +1,6 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+    return <h1 className={styles.heading}> Olá, Mundooo</h1>;
+  //className={styles.heading} é uma forma de adicionar uma classe css ao componente
+}
