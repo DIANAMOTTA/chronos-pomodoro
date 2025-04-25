273e00ff5b1e827a64defe7435689f8107a20c6c
Diana Motta <dianarosamotta@gmail.com>
2025-04-25 20:32:04 -0300

    Criada a Estrutura HTML e CSS para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import { Heading } from './components/Heading';  //importou automaticamente a função Heading
-
 import './styles/theme.css'
 import './styles/global.css'
-import { TimerIcon } from 'lucide-react';
+
 
 export function App () {  //componente função 
-    return ( 
-        <> 
-    <Heading>
-    Olá mundo 1
-    <button>
-        <TimerIcon />
-    </button>
-    </Heading>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias, ratione! Quam est, consequatur magnam nostrum,
-       distinctio rem, debitis laudantium ipsa maiores veniam accusamus illum similique. Amet optio perferendis hic non.
-    </p>
-    </>
-    ); //que retorna um elemento jsx, <> react fragment
+    return <> 
+        <div className="container"> 
+            <div className="content">
+            <section>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam accusantium quo obcaecati ea 
+                doloremque dicta natus enim temporibus! Pariatur nobis ipsam, reprehenderit deleniti asperiores 
+                aliquam dolorem odio eius aspernatur nesciunt?
+            </section>
+            </div>
+        </div>
+
+     
+
+    </> ; //que retorna um elemento jsx, <> react fragment
+    
     
 }
 
